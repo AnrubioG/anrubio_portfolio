0978af79d89dcaf27d36b3fabbd32963422d8ddc
AnubioG <angela.rubio@genstudents.org>
2024-08-12 10:37:40 -0500

    inicio del portafolio

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <header>
+        <div>
+          <p className="text-6xl ">Anrubio Dev</p>
+        </div>
+        <div>
+          <p className="text-6xl ">Home</p>
+          <p className="text-6xl ">Sobre MI</p>
+          <p className="text-6xl ">Proyectos</p>
+          <p className="text-6xl ">Contacto</p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
