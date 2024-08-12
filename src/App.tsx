2289e5f5a0b4042af376bf83ddc62b093940e942
AnubioG <angela.rubio@genstudents.org>
2024-08-12 15:22:58 -0500

    Componente NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <header>
-        <div>
-          <p className="text-6xl ">Anrubio Dev</p>
-        </div>
-        <div>
-          <p className="text-6xl ">Home</p>
-          <p className="text-6xl ">Sobre MI</p>
-          <p className="text-6xl ">Proyectos</p>
-          <p className="text-6xl ">Contacto</p>
-        </div>
-      </header>
+      <Navbar />
+      <section className="max-w-full mx-8 md:mx-28 lg:mx-44 my-20 lg:my-40  flex flex-col items-center md:items-start text-center md:text-left">
+        <h4 className="text-amber-400 font-black text-xl md:text-2xl">
+          HOLA, MI NOMBRE ES ANGELA
+        </h4>
+        <h1 className="text-4xl md:text-6xl lg:text-8xl text-white font-bold mt-4 md:mt-8">
+          Front-End Developer
+        </h1>
+        <p className="text-2xl md:text-2xl max-w-6xl text-white mt-4 md:mt-8">
+          Gracias a mis sólidos conocimientos en herramientas digitales y mi
+          enfoque centrado en el cliente, desarrollo sitios web adaptados a tus
+          necesidades y a las de tus usuarios.
+        </p>
+      </section>
+
+      <section className="max-w-full mx-8 my-20 md:mx-28 lg:mx-44 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12"></section>
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { Dialog, DialogPanel, PopoverGroup } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <nav
+        aria-label="Global"
+        className="mx-8 my-8 md:mx-28 lg:mx-44 flex max-w-full items-center justify-between"
+      >
+        <div className="flex flex-row items-center lg:flex-1 ">
+          <a href="#" className="-m-1.5 p-1.5">
+            <img
+              alt=""
+              src="https://media.licdn.com/dms/image/v2/D4E03AQHJM6X1x_n2vw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718250188894?e=1729123200&v=beta&t=8Tqy_cFB0x5UpI_qhM9MW9VqZisyqu7oPd4rtEfOjSA"
+              className="h-10 w-auto md:h-16 md:w-16 me-5 my-5 rounded-full"
+            />
+          </a>
+          <p className="text-white text-2xl font-bold">Anrubio Dev</p>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+        </div>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-xl font-semibold leading-6 text-white">
+            Home
+          </a>
+          <a href="#" className="text-xl font-semibold leading-6 text-white">
+            Sobre Mi
+          </a>
+          <a href="#" className="text-xl font-semibold leading-6 text-white">
+            Proyectos
+          </a>
+          <a href="#" className="text-xl font-semibold leading-6 text-white">
+            Contacto
+          </a>
+        </PopoverGroup>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img alt="" src="" className="h-8 w-auto" />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
+}
